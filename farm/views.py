c1dc453e03ff3cb61067d97508205b75cc8404b7
Marcio <marcio.grazianni@gmail.com>
2021-08-31 20:38:47 -0300

    Version with map

--- farm/views.py
+++ farm/views.py
@@ -78,104 +78,108 @@ def farm_dashboard(request):
     :param request:
     :return:
     """
-    if 'weather' in request.session.keys():
-        weather_info = request.session['weather']
-    else:
-        generate_location_weather(request)
-        weather_info = request.session['weather']
+    # if 'weather' in request.session.keys():
+    #     weather_info = request.session['weather']
+    # else:
+    #     generate_location_weather(request)
+    #     weather_info = request.session['weather']
 
-    if "farm" not in request.session:
-        farm = Farm.objects.get(
-            member_farms__default_farm=True, member_farms__user_id=request.user.id
-        )
-        request.session["farm"] = str(farm.id)
+    # if "farm" not in request.session:
+    #     farm = Farm.objects.get(
+    #         member_farms__default_farm=True, member_farms__user_id=request.user.id
+    #     )
+    #     request.session["farm"] = str(farm.id)
 
-    try:
-        default_location = Farm.objects.get(pk=request.session["farm"])
-        default_location = default_location.main_location.geo_center
-    except:
-        default_location = [-99.83071945037129, 40.62995241492818]
+    # try:
+    #     default_location = Farm.objects.get(pk=request.session["farm"])
+    #     default_location = default_location.main_location.geo_center
+    # except:
+    #     default_location = [-99.83071945037129, 40.62995241492818]
 
-    today = datetime.today()
-    yesterday = today - timedelta(days=1)
+    # today = datetime.today()
+    # yesterday = today - timedelta(days=1)
 
-    overdue_tasks = (
-        Task.objects.select_related("task_plan__location")
-        .filter(
-            task_plan__location__location_farms__farm__member_farms__user_id=request.user.id
-        )
-        .filter(task_status__name__in=("accepted", "available", "assigned", "declined"))
-        .filter(task_end_date__lt=today)
-    )
-    overdue_tasks_count = overdue_tasks.count()
-    overdue_tasks = overdue_tasks.order_by("-task_end_date")[:5]
+    # overdue_tasks = (
+    #     Task.objects.select_related("task_plan__location")
+    #     .filter(
+    #         task_plan__location__location_farms__farm__member_farms__user_id=request.user.id
+    #     )
+    #     .filter(task_status__name__in=("accepted", "available", "assigned", "declined"))
+    #     .filter(task_end_date__lt=today)
+    # )
+    # overdue_tasks_count = overdue_tasks.count()
+    # overdue_tasks = overdue_tasks.order_by("-task_end_date")[:5]
 
-    in_progress_tasks = (
-        Task.objects.select_related("task_plan__location")
-        .filter(
-            task_plan__location__location_farms__farm__member_farms__user_id=request.user.id
-        )
-        .filter(task_status__name="accepted")
-        .filter(task_end_date__gte=today)
-    )
+    # in_progress_tasks = (
+    #     Task.objects.select_related("task_plan__location")
+    #     .filter(
+    #         task_plan__location__location_farms__farm__member_farms__user_id=request.user.id
+    #     )
+    #     .filter(task_status__name="accepted")
+    #     .filter(task_end_date__gte=today)
+    # )
 
-    task_weather = dict()
+    # task_weather = dict()
 
-    for task in overdue_tasks:
+    # for task in overdue_tasks:
 
-        location = str(task.task_plan.location.id)
-        task_weather[task.id] = weather_info[location]
+    #     location = str(task.task_plan.location.id)
+    #     task_weather[task.id] = weather_info[location]
 
-    in_progress_tasks_count = in_progress_tasks.count()
-    in_progress_tasks = in_progress_tasks.order_by("task_end_date")[:5]
+    # in_progress_tasks_count = in_progress_tasks.count()
+    # in_progress_tasks = in_progress_tasks.order_by("task_end_date")[:5]
 
-    for task in in_progress_tasks:
+    # for task in in_progress_tasks:
 
-        location = task.task_plan.location.id
-        try:
-            task_weather[task.id] = weather_info[location]
-        except KeyError:
-            pass
+    #     location = task.task_plan.location.id
+    #     try:
+    #         task_weather[task.id] = weather_info[location]
+    #     except KeyError:
+    #         pass
 
-    updated_tasks = TaskHistory.objects.select_related(
-        "task_id",
-        "update_user",
-        "task_status",
-        "task_id__task_plan__location",
-        "task_id__task_plan__crop",
-    ).filter(
-        status_date_change__gte=yesterday,
-        task_id__task_plan__location__location_farms__farm_id=request.session["farm"]
-    ).order_by("task_id", "-status_date_change").distinct("task_id")
+    # updated_tasks = TaskHistory.objects.select_related(
+    #     "task_id",
+    #     "update_user",
+    #     "task_status",
+    #     "task_id__task_plan__location",
+    #     "task_id__task_plan__crop",
+    # ).filter(
+    #     status_date_change__gte=yesterday,
+    #     task_id__task_plan__location__location_farms__farm_id=request.session["farm"]
+    # ).order_by("task_id", "-status_date_change").distinct("task_id")
 
-    overdue_work_orders = WorkOrder.objects.filter(
-        farm__id=request.session["farm"]
-    ).filter(end_date__lt=today)
-    overdue_work_orders_count = overdue_work_orders.count()
-    overdue_work_orders = overdue_work_orders.order_by("-end_date")[:5]
+    # overdue_work_orders = WorkOrder.objects.filter(
+    #     farm__id=request.session["farm"]
+    # ).filter(end_date__lt=today)
+    # overdue_work_orders_count = overdue_work_orders.count()
+    # overdue_work_orders = overdue_work_orders.order_by("-end_date")[:5]
 
-    in_progress_work_orders = WorkOrder.objects.filter(
-        farm__id=request.session["farm"]
-    ).filter(end_date__lt=today)
-    in_progress_work_orders_count = in_progress_work_orders.count()
-    in_progress_work_orders = in_progress_work_orders.order_by("end_date")[:5]
+    # in_progress_work_orders = WorkOrder.objects.filter(
+    #     farm__id=request.session["farm"]
+    # ).filter(end_date__lt=today)
+    # in_progress_work_orders_count = in_progress_work_orders.count()
+    # in_progress_work_orders = in_progress_work_orders.order_by("end_date")[:5]
 
+    # return render(
+    #     request,
+    #     "dashboard.html",
+    #     {
+    #         "in_progress_tasks": in_progress_tasks,
+    #         "in_progress_tasks_count": in_progress_tasks_count,
+    #         "in_progress_work_orders": in_progress_work_orders,
+    #         "in_progress_work_orders_count": in_progress_work_orders_count,
+    #         "overdue_tasks": overdue_tasks,
+    #         "overdue_tasks_count": overdue_tasks_count,
+    #         "overdue_work_orders": overdue_work_orders,
+    #         "overdue_work_orders_count": overdue_work_orders_count,
+    #         "task_weather": task_weather,
+    #         "google_key": os.getenv("google_maps_api_key"),
+    #         "map_center_lat": default_location[1],
+    #         "map_center_lng": default_location[0],
+    #         "updated_tasks": updated_tasks,
+    #     },
+    # )
     return render(
         request,
-        "dashboard.html",
-        {
-            "in_progress_tasks": in_progress_tasks,
-            "in_progress_tasks_count": in_progress_tasks_count,
-            "in_progress_work_orders": in_progress_work_orders,
-            "in_progress_work_orders_count": in_progress_work_orders_count,
-            "overdue_tasks": overdue_tasks,
-            "overdue_tasks_count": overdue_tasks_count,
-            "overdue_work_orders": overdue_work_orders,
-            "overdue_work_orders_count": overdue_work_orders_count,
-            "task_weather": task_weather,
-            "google_key": os.getenv("google_maps_api_key"),
-            "map_center_lat": default_location[1],
-            "map_center_lng": default_location[0],
-            "updated_tasks": updated_tasks,
-        },
+        "dashboard.html"
     )
